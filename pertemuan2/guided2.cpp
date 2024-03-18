2d1c1047342febf62aeee26665ba4e7a2b1458f7
RamadhikaPrimadana <163884986+RamadhikaPrimadana@users.noreply.github.com>
2024-03-18 23:29:08 +0700

    add

--- pertemuan2/guided2.cpp
+++ pertemuan2/guided2.cpp
@@ -1,26 +1,47 @@
 #include <iostream>
+
 using namespace std;
+// PROGRAM INPUT ARRAY 3 DIMENSI
 int main()
 {
-int maks, a, i = 1, lokasi;
-cout << "Masukkan panjang array: ";
-cin >> a;
-int array[a];
-cout << "Masukkan " << a << " angka\n";
-for (i = 0; i < a; i++)
-{
-cout << "Array ke-" << (i) << ": ";
-cin >> array[i];
-}
-maks = array[0];
-for (i = 0; i < a; i++)
-{
-if (array[i] > maks)
-{
-maks = array[i];
-lokasi = i;
-}
-}
-cout << "Nilai maksimum adalah " << maks << " berada di Array
-ke " << lokasi << endl;
+    // Deklarasi array
+    int arr[2][3][3];
+    // Input elemen
+    for (int x = 0; x < 2; x++)
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            for (int z = 0; z < 3; z++)
+            {
+                cout << "Input Array[" << x << "][" << y << "][" << z << "] = ";
+                cin >> arr[x][y][z];
+            }
+        }
+        cout << endl;
+    }
+    // Output Array
+    for (int x = 0; x < 2; x++)
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            for (int z = 0; z < 3; z++)
+            {
+                cout << "Data Array[" << x << "][" << y << "][" << z << "] = " << arr[x][y][z] << endl;
+            }
+        }
+    }
+    cout << endl;
+    // Tampilan array
+    for (int x = 0; x < 2; x++)
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            for (int z = 0; z < 3; z++)
+            {
+                cout << arr[x][y][z] << ends;
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
 }
